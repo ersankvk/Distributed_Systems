2d267000b06331e618460bb329e1ebe0570ae9a2
if23b090 <if23b090@technikum-wien.at>
2025-05-26 11:37:00 +0200

    added dependencies in pom.xml

--- energy-community-user/src/main/java/fhtw/at/energy_community_user/EnergyCommunityUserApplication.java
+++ energy-community-user/src/main/java/fhtw/at/energy_community_user/EnergyCommunityUserApplication.java
@@ -1,11 +1,13 @@
 package fhtw.at.energy_community_user;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRabbit
 
 public class EnergyCommunityUserApplication {
 
--- energy-community/src/main/java/fhtw/at/energy_community/EnergyCommunityApplication.java
+++ energy-community/src/main/java/fhtw/at/energy_community/EnergyCommunityApplication.java
@@ -1,11 +1,14 @@
 package fhtw.at.energy_community;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRabbit
+
 public class EnergyCommunityApplication {
 
 	public static void main(String[] args) {
