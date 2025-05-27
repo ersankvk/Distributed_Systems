ace01dfe26eaa43268b19ecb8c77d67b4a5b53d0
if23b090 <if23b090@technikum-wien.at>
2025-05-27 16:08:21 +0200

    enabled scheduled processing

--- energy-community-percentage_service/src/main/java/fhtw/wien/energy_community_percentage_service/EnergyCommunityPercentageServiceApplication.java
+++ energy-community-percentage_service/src/main/java/fhtw/wien/energy_community_percentage_service/EnergyCommunityPercentageServiceApplication.java
@@ -3,9 +3,11 @@ package fhtw.wien.energy_community_percentage_service;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableScheduling
 public class EnergyCommunityPercentageServiceApplication {
 
 	public static void main(String[] args) {
